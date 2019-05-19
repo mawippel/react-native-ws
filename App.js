b29e18ae2d595d5039ed2c086bbbb6e1fb941b9a
Marcelo Wippel <mawippel2@hotmail.com>
2019-05-19 11:52:07 -0300

    feat: getting ws data and storing it

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import axios from 'axios'
+import { StyleSheet, Text, ScrollView, ActivityIndicator, AsyncStorage } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
+
+  state = {
+    breeds: null
+  }
+
+  async componentDidMount() {
+    try {
+      let breeds = await axios.get('https://dog.ceo/api/breed/pinscher/images')
+      if (breeds.data.message) {
+        this.setState({ breeds: breeds.data.message });
+        await this.storeData();
+        await this.getKey();
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  storeData = async () => {
+    try {
+      await AsyncStorage.setItem('@BeatifulPinschers', `${this.state.breeds[0]}`);
+    } catch (error) {
+      console.log()
+    }
+  };
+
+  getKey = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@BeatifulPinschers');
+      this.setState({myKey: value});
+      console.log(this.state.myKey);
+    } catch (error) {
+      console.log("Error retrieving data" + error);
+    }
+  }
+
   render() {
+    if (!this.state.breeds) {
+      return <ActivityIndicator size="large" color="#0000ff" />
+    }
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <ScrollView style={styles.container}>
+        <Text style={{textAlign:'center'}}>FOTOS DE PINSCHER</Text>
+        {this.state.breeds.map((b, index) => {
+          return (<Text key={index}>{b}</Text>)
+        })}
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical: 20,
+    backgroundColor: '#F5FCFF'
   },
 });
